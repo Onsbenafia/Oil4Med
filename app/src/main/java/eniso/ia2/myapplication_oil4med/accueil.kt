f1431cf092638c5f4ad36ce757fed450f1b5970a
Onsbenafia <108668887+Onsbenafia@users.noreply.github.com>
2023-03-23 13:34:18 +0100

    firt commit
    23/03/2023

---
+++ app/src/main/java/eniso/ia2/myapplication_oil4med/accueil.kt
@@ -0,0 +1,12 @@
+package eniso.ia2.myapplication_oil4med
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class accueil : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_accueil)
+
+    }
+}
